1d9928d7d9eeeaa07442ee37dc8d50954db3119f
SnowBit <dtshah24022006@gmail.com>
2021-11-15 12:49:31 +0530

    Added gradient button

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const dark = document.getElementById('dark')
 const light = document.getElementById('light')
 const catty = document.getElementById('cattyName')
+const btn = document.getElementById('btn')
 
 function toggleDark(){
     dark.classList.add('d-none')
     light.classList.remove('d-none')
+    btn.classList.add('btn-light')
+    btn.classList.add('color-8')
+    btn.classList.remove('color-9')
+    btn.classList.remove('btn-dark')
     document.body.style.backgroundColor = "#212224"
     document.body.style.color = "#fff"
     catty.style.color = "#fff"
@@ -15,6 +20,10 @@ dark.addEventListener('click', toggleDark)
 function toggleLight(){
     light.classList.add('d-none')
     dark.classList.remove('d-none')
+    btn.classList.remove('color-8')
+    btn.classList.add('color-9')
+    btn.classList.remove('btn-light')
+    btn.classList.add('btn-dark')
     document.body.style.backgroundColor = "#fff"
     document.body.style.color = "#000"
     catty.style.color = "#000"
