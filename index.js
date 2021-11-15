5972d089d5f3c1a2409c5aa49c689c278040d60b
SnowBit <dtshah24022006@gmail.com>
2021-11-15 17:58:19 +0530

    Changed button color

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const btn = document.getElementById('btn')
 function toggleDark(){
     dark.classList.add('d-none')
     light.classList.remove('d-none')
-    btn.classList.add('btn-light')
-    btn.classList.add('color-8')
-    btn.classList.remove('color-9')
-    btn.classList.remove('btn-dark')
+    // btn.classList.add('btn-light')
+    // btn.classList.add('btn-outline-warning')
+    // btn.classList.remove('btn-outline-dark')
+    // btn.classList.remove('btn-dark')
     document.body.style.backgroundColor = "#212224"
     document.body.style.color = "#fff"
     catty.style.color = "#fff"
+    // btn.style.color = "#fff"
 }
 
 dark.addEventListener('click', toggleDark)
@@ -20,10 +21,10 @@ dark.addEventListener('click', toggleDark)
 function toggleLight(){
     light.classList.add('d-none')
     dark.classList.remove('d-none')
-    btn.classList.remove('color-8')
-    btn.classList.add('color-9')
-    btn.classList.remove('btn-light')
-    btn.classList.add('btn-dark')
+    // btn.classList.remove('color-8')
+    // btn.classList.add('btn-outline-dark')
+    // btn.classList.remove('btn-outline-warning')
+    // btn.classList.add('btn-dark')
     document.body.style.backgroundColor = "#fff"
     document.body.style.color = "#000"
     catty.style.color = "#000"
